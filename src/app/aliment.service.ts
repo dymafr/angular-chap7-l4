27872ee8457a1e8bbae046b710db402a9f326b1b
dymafr <erwan1309@hotmail.com>
2020-12-13 15:38:54 +0100

    end

--- src/app/aliment.service.ts
+++ src/app/aliment.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from "@angular/core";
+import { LogService } from "./log.service";
 
-@Injectable()
+@Injectable({ providedIn: "root" })
 export class AlimentService {
   public aliments: string[] = [];
 
-  constructor() {}
+  constructor(private logService: LogService) {}
 
   public addOne(aliment: string): void {
     this.aliments.push(aliment);
+    this.logService.log("Aliment added");
   }
 
   public removeOne(index: number): void {
     this.aliments.splice(index, 1);
+    this.logService.log("Aliment removed");
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { AddAlimentComponent } from "./add-aliment/add-aliment.component";
 import { ListAlimentComponent } from "./list-aliment/list-aliment.component";
-import { AlimentService } from "./aliment.service";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, AddAlimentComponent, ListAlimentComponent],
-  bootstrap: [AppComponent],
-  providers: [AlimentService]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
