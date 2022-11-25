f8e6eb66fdcc44fda04da99f6969f7982e04f798
dymafr <32801166+dymafr@users.noreply.github.com>
2022-11-25 12:22:04 +0100

    4 files modified, 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import "./polyfills";
-
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-
-import { AppModule } from "./app/app.module";
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'zone.js'; // Nécessaire uniquement pour Stackblitz
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
